cf74be9ccc9e6dfc9849b0375e3a5423f34a2a97
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-03-30 00:11:17 +0000

    Refactor jsconfig.json and vite.config.js to simplify path aliases for improved maintainability

--- weath2r/vite.config.js
+++ weath2r/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,19 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src',
-      '@components': path.resolve(__dirname, '/src/components'),
-      '@components-layout': path.resolve(__dirname, '/src/components/layout'),
-      '@components-weather': path.resolve(__dirname, '/src/components/weather'),
-      '@context': path.resolve(__dirname, '/src/context'),
-      '@pages': path.resolve(__dirname, '/src/pages'),
-      '@services': path.resolve(__dirname, '/src/services'),
-      '@services-api': path.resolve(__dirname, '/src/services/api'),
-      '@styles': path.resolve(__dirname, '/src/styles'),
-      '@styles-base': path.resolve(__dirname, '/src/styles/base'),
-      '@styles-components': path.resolve(__dirname, '/src/styles/components'),
-      '@styles-layout': path.resolve(__dirname, '/src/styles/components/layout'),
-      '@styles-weather': path.resolve(__dirname, '/src/styles/components/weather'),
-      '@styles-pages': path.resolve(__dirname, '/src/styles/pages'),
     },
   },
 })
